2fa339d9a7ff5e9c326f2b88b235ada80dc0145d
AKArunkumar <arunkumar717.at@gmail.com>
2018-04-26 02:50:35 +0530

    added email auth config to App component ngOnit hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   selectedfeature = 'recipe';
-
+ 
+  ngOnInit() {
+    firebase.initializeApp({
+        apiKey: "AIzaSyDhBwf33j-0eEEzCXpmMJgLNBIOJBoLBtQ",
+        authDomain: "recipe-book-c6b58.firebaseapp.com"
+      }
+    );
+  }
   featureload(feature: string) {
     this.selectedfeature = feature;
   }
