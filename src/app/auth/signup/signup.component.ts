8456372897b01f5143b4c28bf58d3694134111c3
AKArunkumar <arunkumar717.at@gmail.com>
2018-04-26 03:09:43 +0530

    added signup method and signup successful

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authservice: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onSingup(form: NgForm) {
+    console.log('hi');
+    const email = form.value.email;
+    const password = form.value.password;
+    this.authservice.userSignup(email, password);
   }
 
 }
