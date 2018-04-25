0cf4984daab3457a8128f3f9d95acdcf4c4a3667
AKArunkumar <arunkumar717.at@gmail.com>
2018-04-26 03:07:58 +0530

    auth serive to singup user

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Injectable()
-export class AuthService {
-
+export class AuthService  {
   constructor() { }
+
+  userSignup(email: string, password: string) {
+    firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password)
+            .catch((error) => {
+              console.log(error);
+            })
+  }
 
 }
