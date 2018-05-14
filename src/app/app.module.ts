e9a8f4d29786d0b8a0f01e71b4f070eaf38cdb00
AKArunkumar <arunkumar717.at@gmail.com>
2018-05-14 23:24:50 +0530

    cleaned app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,12 @@ import { RecipeService } from './service/recipe.service';
 import { AppRoutingModule } from './app-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { FirebaseService } from './service/firebase.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGaurdService } from './auth/auth-gaurd.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shoppingList.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
@@ -24,17 +23,15 @@ import { ShoppingListModule } from './shopping-list/shoppingList.module';
     AppComponent,
     HeaderComponent,
     NotFoundComponent,
-    SignupComponent,
-    SigninComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     AppRoutingModule,
     RecipesModule,
     SharedModule,
-    ShoppingListModule
+    ShoppingListModule,
+    AuthModule
   ],
   providers: [ShoppingService, RecipeService, FirebaseService, AuthService, AuthGaurdService],
   bootstrap: [AppComponent]
