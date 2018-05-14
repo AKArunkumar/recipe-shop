a6c2d49286155d844a8cd9810bc7168cf537ac89
AKArunkumar <arunkumar717.at@gmail.com>
2018-05-14 22:58:51 +0530

    added shared module and clean the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropmenuDirective } from './dropmenu/dropmenu.directive';
 import { ShoppingService } from './service/shopping.service';
 import { RecipeService } from './service/recipe.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { FirebaseService } from './service/firebase.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGaurdService } from './auth/auth-gaurd.service';
+import { RecipesModule } from './recipes/recipes.module';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shoppingList.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    DropmenuDirective,
-    RecipeStartComponent,
     NotFoundComponent,
-    RecipeEditComponent,
     SignupComponent,
-    SigninComponent
+    SigninComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    RecipesModule,
+    SharedModule,
+    ShoppingListModule
   ],
   providers: [ShoppingService, RecipeService, FirebaseService, AuthService, AuthGaurdService],
   bootstrap: [AppComponent]
