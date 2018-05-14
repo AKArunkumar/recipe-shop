d7c12610361e475cdc5a4d662ad5754355421f22
AKArunkumar <arunkumar717.at@gmail.com>
2018-05-14 22:58:27 +0530

    clean the code form app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,35 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropmenuDirective } from './dropmenu/dropmenu.directive';
-import { ShoppingService } from './service/shopping.service';
-import { RecipeService } from './service/recipe.service';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
-import { AuthGaurdService } from './auth/auth-gaurd.service';
 
-const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent, children: [
-        { path: '', component: RecipeStartComponent },
-        { path: 'new', component: RecipeEditComponent, canActivate: [AuthGaurdService] },
-        { path: ':id', component: RecipeDetailComponent },
-        { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGaurdService]}
-    ] },
+const appRoutes: Routes = [   
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },         
     { path: 'shoping-list', component: ShoppingListComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'signin', component: SigninComponent },
-    { path: 'not-found', component: NotFoundComponent },
-    { path: '**', redirectTo: '/not-found'}
+    { path: 'not-found', component: NotFoundComponent }
 ];
 
 @NgModule( {
